6985b52244de8dde7551dcb488ce625d5906753e
savio <saviojairo@gmail.com>
2026-06-13 18:44:25 -0300

    versao copa 2026 versao sem anuncio e nova home que atualiza

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -1,6 +1,6 @@
 import HomeClient from "./client"
 import { api } from "@/app/servidor/api"
-
+export const dynamic = "force-dynamic"
 export default async function HomePage() {
   const res = await api.get("/home")
   const data = res.data
