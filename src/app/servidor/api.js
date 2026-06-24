33d1046fafae1d1e4787b6d94310f50d72c06ab1
savio <saviojairo@gmail.com>
2026-06-24 15:44:54 -0300

    versao copa 2026 1.0

--- src/app/servidor/api.js
+++ src/app/servidor/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-// const baseURL = "https://api.futlance.com"
-const baseURL = "http://localhost:3333"
+const baseURL = "https://api.futlance.com"
 export const api = axios.create({
   baseURL,
   timeout: 10000,
 })
-export const wsUrl = "ws://localhost:3333/ws"
-// export const wsUrl = "wss://api.futlance.com/ws"
+export const wsUrl = "wss://api.futlance.com/ws"
